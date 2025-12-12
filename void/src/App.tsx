8e02144ec1d110176cf3abbc8d4b72ccd3636f4d
benjamin olmsted <benjamin.olmsted@gmail.com>
2025-12-11 16:49:04 -0800

    LINTERRRRRRR

--- void/src/App.tsx
+++ void/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import { supabase } from './lib/supabase'
-import { stripePromise } from './lib/stripe'
 
 interface Pixel {
   x: number
